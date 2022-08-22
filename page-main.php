afb2f98718851496f572564c642f5aa0ff727b58
zeein <zeein81@gmail.com>
2022-08-22 13:48:06 +0900

    first commit

---
+++ page-main.php
@@ -0,0 +1,14 @@
+<?php
+get_header();
+?>
+page-main.php
+
+<div class="page-main mx-auto container px-6 md:text-red-600 lg:text-blue-400 text-xs md:text-2xl lg:text-6xl bg-slate-200 ">
+  <h2 class="text-primary/50 font-bold">skflsd</h2>
+  <h2 class="text-primary/30 font-bold">skflsd</h2>
+  <h3 class="text-secondary">소제목</h3>
+</div>
+
+<?php
+get_footer();
+?>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.php'],
+  theme: {
+    colors: {
+      primary: '#ff0000',
+      secondary: '#ff6600',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
